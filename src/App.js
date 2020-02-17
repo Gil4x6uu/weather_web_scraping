a234901e172dbd189405b90bb97b2cd7975cb695
Gil Gross <gil4x6uu@gmail.com>
2020-02-17 21:50:06 +0200

    Berfore server app.get remake

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const uniqid = require('uniqid');
+let myIntaerval = null;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      weatherLocations: [],
+      cityToSearch: "",
+      searchCity: ""
+    }
+  }
+  inputHandler = (event) => {
+    this.setState({ cityToSearch: event.target.value })
+  };
+
+  handleClick = () => {
+    this.getCityForcast(this.state.cityToSearch);
+    if (myIntaerval !== null) clearInterval(myIntaerval);
+    myIntaerval = setInterval(this.getCityForcast, 60000, this.state.cityToSearch);
+
+  }
+
+  componentDidMount() {   
+    this.updateConstCitysForcast();
+    setInterval(this.updateConstCitysForcast, 10000);
+  }
+
+  getCityForcast = (cityName) => {
+    axios.get("http://localhost:3000/getweather?name=" + cityName)
+      .then((res) => {
+        this.setState({ searchCity: res.data })
+        console.log("Search");
+      })
+  }
+  
+  updateConstCitysForcast = () => {
+    axios.get("http://localhost:3000/pageLoad")
+      .then((res) => {
+        this.setState({ weatherLocations: res.data })
+        console.log("Cities");
+      })
+  }
+
+
+
+  render() {
+    return (
+      <React.Fragment>
+      <div>
+          <input type="text" onChange={this.inputHandler} />
+          <button onClick={this.handleClick} className ="button">SEARCH </button>
+      </div>
+        <div className="weatherCard">
+          <h3>{this.state.searchCity.Name}</h3>
+          <br />
+          {this.state.searchCity.Temp}
+          <br />
+          {this.state.searchCity.Humidity}
+          <br />
+          {this.state.searchCity.Wind}
+          <br />
+          <img src={this.state.searchCity.Image} alt={this.state.searchCity.ImageDescs} />
+          <br />
+          {this.state.searchCity.ImageDesc}
+          <br />
+          <br />
+        </div>
+        <div className ="weatherCard">
+        {this.state.weatherLocations.map(location =>
+          <React.Fragment key={uniqid()} >
+            <h3>{location.Name}</h3>
+            <br />
+             {location.Temp}°C
+            <br />
+            {location.Humidity}
+            <br />
+            {location.Wind}
+            <br />
+            <img src={location.Image} alt={location.ImageDesc} />
+            <br />
+            {location.ImageDesc}
+            <br />
+            <br />
+          </React.Fragment>)}
+      </div>
+      </React.Fragment>
+    );
+  }
 }
 
+
+
 export default App;
+
